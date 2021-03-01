6f7142fda86c58b83980682b780f642792284de7
Jongkil Park <park108@Jongkilui-MacBookAir.local>
2021-03-01 15:34:08 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+				park108.net
+				</p>
+				<p>
+				만드는 중
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
