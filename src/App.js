ba3624186af0cf78205cc3df3c60a8c8ea897247
Jongkil Park <park108@Jongkilui-MacBookAir.local>
2021-03-02 08:27:14 +0900

    Add nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Log from "./Log/Log";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-				park108.net
-				</p>
-				<p>
-				만드는 중
-				</p>
-			</header>
-		</div>
-	);
+const menuList = {
+	0: <Log />,
+};
+  
+class App extends React.Component {
+
+	constructor(props) {
+
+		super();
+	
+		this.state = {
+			menu: 0,
+		};
+	}
+	
+	changeMenu = (menuIndex) =>{
+		this.setState({menu : menuIndex});
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="nav-bar">
+					<ul className="tabs">
+						<li className="nav-bar-title"><a href="/">park108.net</a></li>
+						<li className={`${this.state.menu === 0? 'active': ''}`} onClick={() => this.changeMenu(0)}>log</li>
+						<li><a href="https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000/">login</a></li>
+					</ul>
+				</div>
+				<div className="contents">
+					{menuList[this.state.menu]}
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/Log/Log.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Log extends Component {
+
+	constructor(props) {
+
+		super();
+
+		this.state = {
+			menu: 0,
+		};
+	}
+
+	render() {
+
+		return (
+			<div>
+				<p>log</p>
+				<p>만드는 중.</p>
+			</div>
+		);
+	}
+}
+
+export default Log;
