89bf5a7de942912b206a63ad1490d656e7bd9d22
Jongkil Park <park108@Jongkilui-MacBookAir.local>
2021-03-21 04:49:36 +0900

    Add write form

--- src/App.js
+++ src/App.js
@@ -1,61 +1,55 @@
-import React, { Component } from "react";
-import './App.css';
+import React from "react";
 import Log from "./Log/Log";
+import './App.css';
 
 const menuList = {
 	0: <Log />,
 };
-  
-class App extends Component {
 
-	constructor(props) {
 
-		super();
-	
-		this.state = {
-			menu: 0,
-		};
+const getUrl = () => {
+	if (process.env.NODE_ENV === 'production') {
+		return "https://park108.net/";
 	}
+	else if (process.env.NODE_ENV === 'development') {
+		return "http://localhost:3000/";
+	}
+}
+
+// TODO: Change url after make Cognito User Pool for production
+const getLoginUrl = () => {
+	if (process.env.NODE_ENV === 'production') {
+		return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000";
+	}
+	else if (process.env.NODE_ENV === 'development') {
+		return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000";
+	}
+}
+  
+const App = () => {
 	
-	changeMenu = (menuIndex) =>{
+	const state = {
+		menu: 0,
+	};
+	
+	const changeMenu = (menuIndex) => {
 		this.setState({menu : menuIndex});
 	}
 
-	getUrl () {
-		if (process.env.NODE_ENV === 'production') {
-			return "https://park108.net/";
-		}
-		else if (process.env.NODE_ENV === 'development') {
-			return "http://localhost:3000/";
-		}
-	}
-
-	// TODO: Change url after make Cognito User Pool for production
-	getLoginUrl () {
-		if (process.env.NODE_ENV === 'production') {
-			return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000";
-		}
-		else if (process.env.NODE_ENV === 'development') {
-			return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000";
-		}
-	}
-
-	render() {
-		return (
-			<div>
-				<div className="nav-bar">
-					<ul className="tabs">
-						<li className="nav-bar-title"><a href={this.getUrl()}>park108.net</a></li>
-						<li className={`${this.state.menu === 0? 'active': ''}`} onClick={() => this.changeMenu(0)}>log</li>
-						<li><a href={this.getLoginUrl() }>login</a></li>
-					</ul>
-				</div>
-				<div className="contents">
-					{menuList[this.state.menu]}
-				</div>
+	return (
+		<div>
+			<div className="nav-bar">
+				<ul className="tabs">
+					<li className="nav-bar-title"><a href={getUrl()}>park108.net</a></li>
+					<li className={`${state.menu === 0? 'active': ''}`} onClick={() => changeMenu(0)}>log</li>
+					<li><a href={getLoginUrl() }>login</a></li>
+				</ul>
 			</div>
-		)
-	}
+			<div className="contents">
+				{menuList[state.menu]}
+			</div>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/Log/Writer.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function sleep(ms) {
+	console.log("Processing...");
+	return new Promise((r) => setTimeout(r, ms));
+}
+
+const Writer = (props) => {
+
+	const [article, setArticle] = useState("");
+	const [disabled, setDisabled] = useState(false);
+
+	const handleChange = ({ target: { value } }) => setArticle(value);
+
+	const handleSubmit = (event) => {
+		
+		setDisabled(true);
+
+		if(article.length < 5) {
+
+			alert("Please note at least 5 characters.");
+		}
+		else {
+
+			event.preventDefault();
+			console.log(`Submitted contents: ${article}`);
+			props.onSubmit(article);
+
+			sleep(1000).then(() => {
+				console.log("done");
+				setDisabled(false);
+			});
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<textarea
+				className="textarea--article-normal"
+				type="text"
+				name="article"
+				value={article}
+				onChange={handleChange}
+				placeholder="Take your note"
+			/>
+			<button
+				className="button--submit-normal"
+				type="submit"
+				disabled={disabled}
+			>Post</button>
+		</form>
+	);
+}
+
+export default Writer;
