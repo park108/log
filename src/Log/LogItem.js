10be761a93df16ccfe357fe3f4a4ec5b334f3743
Jongkil Park <park108@Jongkilui-MacBookAir.local>
2021-04-11 08:58:18 +0900

    Change css

--- src/Log/LogItem.js
+++ src/Log/LogItem.js
@@ -3,31 +3,6 @@ import * as common from '../common';
 
 const LogItem = (props) => {
 
-	const item = props.item;
-
-	let output = common.convertToHTML(item.logs[0].contents);
-
-	let dateText = "";
-
-	if(item.timestamp > 0) {
-
-		let date = new Date(item.timestamp);
-
-		let yyyy = date.getFullYear();
-		let mm = date.getMonth() + 1;
-		let dd = date.getDate();
-		let hh = date.getHours();
-		let min = date.getMinutes();
-		let ss = date.getSeconds();
-
-		dateText = yyyy + "-"
-			+ (mm < 10 ? "0" + mm : mm) + "-"
-			+ (dd < 10 ? "0" + dd : dd) + " "
-			+ (hh < 10 ? "0" + hh : hh) + ":"
-			+ (min < 10 ? "0" + min : min) + ":"
-			+ (ss < 10 ? "0" + ss : ss);
-	}
-
 	const editLogItem = () => {
 
 		// Go to Edit URL
@@ -57,18 +32,62 @@ const LogItem = (props) => {
 		});
 	}
 
+	const item = props.item;
+
+	const outputContents = common.convertToHTML(item.logs[0].contents);
+
+	let outputDate = "";
+	let outputTime = "";
+
+	if(item.timestamp > 0) {
+
+		let date = new Date(item.timestamp);
+
+		let yyyy = date.getFullYear();
+		let mm = date.getMonth() + 1;
+		let dd = date.getDate();
+		let hh = date.getHours();
+		let min = date.getMinutes();
+		let ss = date.getSeconds();
+
+		outputDate = yyyy + "-"
+			+ (mm < 10 ? "0" + mm : mm) + "-"
+			+ (dd < 10 ? "0" + dd : dd);
+		outputTime = " "
+			+ (hh < 10 ? "0" + hh : hh) + ":"
+			+ (min < 10 ? "0" + min : min) + ":"
+			+ (ss < 10 ? "0" + ss : ss);
+
+
+		outputDate = <span>{outputDate}</span>;
+		outputTime = <span>{outputTime}</span>;
+	}
+
+	let outputAuthor = "";
+	let infoSeparator = "";
+	if(common.isAdmin()) {
+		outputAuthor = <span>{item.author}</span>;
+		infoSeparator = <span className="span span--article-separator">|</span>;
+	}
+	else {
+		outputTime = "";
+	}
+
 	return (
 		<div className="div div--article-logitem">
-			<p dangerouslySetInnerHTML={{__html: output}}></p>
+			<p dangerouslySetInnerHTML={{__html: outputContents}}></p>
+			<p className="p p--article-info">
+				{outputAuthor}
+				{infoSeparator}
+				{outputDate}
+				{outputTime}
+			</p>
 			{common.isAdmin() &&
-			<div>
-			<p className="p p--article-info">{item.author}, {dateText}</p>
 			<p className="p p--article-toolbar">
 				<span onClick={editLogItem} className="span span--article-toolbar">Edit</span>
 				<span className="span span--article-separator">|</span>
 				<span onClick={deleteLogItem} className="span span--article-toolbar">Delete</span>
 			</p>
-			</div>
 			}
 		</div>
 	)
