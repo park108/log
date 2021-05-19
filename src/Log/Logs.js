bbf0786313aa6681e1d373c15b5edbbf1b3f369c
Jongkil Park <park108@Jongkilui-MacBookAir.local>
2021-05-19 09:37:12 +0900

    Add loading text

--- src/Log/Logs.js
+++ src/Log/Logs.js
@@ -6,14 +6,19 @@ import LogItem from './LogItem';
 const Logs = (props) => {
 
 	const [logs, setLogs] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [loading, setLoading] = useState(null);
 
 	async function fetchData() {
+
+		setIsLoading(true);
 
 		// Call GET API
 		const res = await fetch(common.getAPI());
 		
 		res.json().then(res => {
 			console.log("DATA FETCHED from AWS!!");
+			setIsLoading(false);
 			setLogs(res.body.Items);
 		}).catch(err => {
 			console.error(err);
@@ -23,6 +28,15 @@ const Logs = (props) => {
 	useEffect(() => {
 		fetchData();
 	}, [props.isPostSuccess]);
+
+	useEffect(() => {
+		if(isLoading) {
+			setLoading(<div className="div div--logs-loading">Loading...</div>);
+		}
+		else {
+			setLoading(null);
+		}
+	}, [isLoading]);
 
 	const writeLink = common.isAdmin() ? <Link
 			to={{pathname: "/log/write"
@@ -36,16 +50,17 @@ const Logs = (props) => {
 	return (
 		<div className="div div--logs-main">
 			{writeLink}
-		{logs.map(data => (
-			<LogItem
-				key={data.timestamp}
-				author={data.author}
-				timestamp={data.timestamp}
-				contents={data.logs[0].contents}
-				item = {data}
-				delete={fetchData}
-			/>
-		))}
+			{logs.map(data => (
+				<LogItem
+					key={data.timestamp}
+					author={data.author}
+					timestamp={data.timestamp}
+					contents={data.logs[0].contents}
+					item = {data}
+					delete={fetchData}
+				/>
+			))}
+			{loading}
 		</div>
 	);
 }
