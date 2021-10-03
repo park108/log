164c2a54a2f940b10b3da9364e43bb528204c22d
Jongkil Park <park108@gmail.com>
2021-10-03 23:34:39 +0900

    Add Toaster unit test

---
+++ src/Toaster/Toaster.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import Toaster from './Toaster';
+
+it('render message text correctly', () => {
+  render(<Toaster 
+    message="Test message"
+  />);
+  const title = screen.getByText("Test message");
+  expect(title).toBeInTheDocument();
+});
+
+it('render no show', () => {
+  render(<Toaster 
+    message={"Test message"}
+    show={0}
+  />);
+  const toaster = screen.getByText("Test message");
+  expect(toaster).toHaveAttribute('class', 'div div--toaster-hide');
+});
+
+it('render information in center', () => {
+  render(<Toaster 
+    message={"Test message"}
+    position={"center"}
+    show={1}
+  />);
+  const toaster = screen.getByText("Test message");
+  expect(toaster).toHaveAttribute('class', 'div div--toaster-center div--toaster-information ');
+});
+
+it('render success in bottom', () => {
+  render(<Toaster 
+    message={"Test message"}
+    position={"bottom"}
+    type={"success"}
+    show={1}
+  />);
+  const toaster = screen.getByText("Test message");
+  expect(toaster).toHaveAttribute('class', 'div div--toaster-bottom div--toaster-success ');
+});
