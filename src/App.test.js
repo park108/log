aff44d1c7b3effbc613b4aa8b7abaa6e8b7689e6
Jongkil Park <park108@gmail.com>
2021-10-03 23:04:17 +0900

    Init JEST test App.test.js for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeAll(() => {
+  delete window.location;
+  window.location = {
+      href: '',
+  };
+});
+
+test('render title text correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/park108.net/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('render linkedin link correctly', () => { 
+  render(<App />);
+  const anchor = screen.getByText('[in]').closest('a');
+  expect(anchor).toHaveAttribute('href', 'https://www.linkedin.com/in/jongkil-park-48019576/');
+});
+
+test('render github link correctly', () => {
+  render(<App />);
+  const anchor = screen.getByText('[git]').closest('a');
+  expect(anchor).toHaveAttribute('href', 'https://github.com/park108');
 });
