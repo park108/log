738bb5d93e3ab666ccccc2c612f10cb4ee024ac0
Jongkil Park <park108@gmail.com>
2021-10-15 10:31:03 +0900

    Add powered by aws logo in footer

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,11 @@ const Footer = (props) => {
 					© 2021 Jongkil Park
 				</span>
 				<span className="span span--footer-right">
+					<a href="http://aws.amazon.com/what-is-cloud-computing">
+						<img src="https://d0.awsstatic.com/logos/powered-by-aws.png" alt="Powered by AWS Cloud Computing" height="35px" />
+					</a>
+				</span>
+				<span className="span span--footer-right">
 					<a href="https://www.linkedin.com/in/jongkil-park-48019576/" rel="noreferrer" target="_blank">[in]</a>
 					&nbsp;
 					<a href="https://github.com/park108" rel="noreferrer" target="_blank">[git]</a>
