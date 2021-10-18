318a6aadf6268058aa0e5d26b06994afe06e304f
Jongkil Park <park108@gmail.com>
2021-10-18 23:42:24 +0900

    Add dummy data

--- src/Monitor/ApiMon.js
+++ src/Monitor/ApiMon.js
@@ -3,6 +3,12 @@ import React from "react";
 const ApiMon = (props) => {
 	return <div className="div div--article-logitem">
 		<h3>APIs</h3>
+		<div className="div div--monitor-item">
+			<h4>Invocations</h4>
+		</div>
+		<div className="div div--monitor-item">
+			<h4>Duration</h4>
+		</div>
 	</div>
 }
 
