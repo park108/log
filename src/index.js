65cadfd7968ad719bafd4bef74770477d02fa345
Jongkil Park <park108@gmail.com>
2021-11-04 22:49:20 +0900

    Add ua sendBeacon

--- src/index.js
+++ src/index.js
@@ -29,8 +29,57 @@ reportWebVitals(sendToAnalytics);
 
 // Send visitor info to analytics endpoint.
 function sendCounter() {
-  // TODO: Send data through sendBeacon() to service endpoint.
-  log(navigator.userAgent);
+
+  let uaText = navigator.userAgent;
+
+  // Parser reference: https://developer.mozilla.org/ko/docs/Web/HTTP/Browser_detection_using_the_user_agent
+  // Browser
+  let browser = uaText.indexOf("Firefox/") > -1? "Firefox"
+    : uaText.indexOf("Seamonkey/") > -1 ? "Seamonkey"
+    : uaText.indexOf("Chrome/") > -1 ? "Chrome"
+    : uaText.indexOf("Chromium/") > -1 ? "Chromium"
+    : uaText.indexOf("Safari/") > -1 ? "Safari"
+    : uaText.indexOf("OPR/") > -1 ? "Opera"
+    : uaText.indexOf("Opera/") > -1 ? "Opera"
+    : uaText.indexOf("; MSIE ") > -1 ? "Internet Explorer"
+    : "Others";
+
+
+  // Rendering engine
+  let renderingEngine = uaText.indexOf("Gecko/") > -1 ? "Gecko"
+    : uaText.indexOf("AppleWebKit/") > -1 ? "Webkit"
+    : uaText.indexOf("Opera/") > -1 ? "Presto"
+    : uaText.indexOf("Trident/") > -1 ? "Trident"
+    : uaText.indexOf("Chrome/") > -1 ? "Blink"
+    : "Others";
+
+  // Operating system
+  let operatingSystem = uaText.indexOf("Android") > -1? "Android"
+    : uaText.indexOf("iPhone OS") > -1 ? "iOS"
+    : uaText.indexOf("Windows") > -1 ? "Windows"
+    : uaText.indexOf("Mac OS X") > -1 ? "Mac OS X"
+    : uaText.indexOf("(X11; CrOS") > -1 ? "Chrome OS"
+    : uaText.indexOf("X11") > -1 ? "Linux"
+    : uaText.indexOf("Symbian") > -1 ? "Symbian"
+    : "Others";
+
+  // Make posting data
+  const userAgentInfo = {
+    originalText: uaText,
+    browser: browser,
+    renderingEngine: renderingEngine,
+    operatingSystem: operatingSystem
+  }
+
+  log(userAgentInfo);
+
+  const url = commonMonitor.getAPI() + "/useragent";
+  const body = JSON.stringify(userAgentInfo);
+
+  if(navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  }
+
 }
 
 sendCounter();
