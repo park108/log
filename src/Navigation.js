615bd84c9a5dc02a79c1ce7b7bd8d45f8d2c72b0
Jongkil Park <park108@gmail.com>
2021-12-08 23:35:28 +0900

    Refactor Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,58 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link, useLocation } from 'react-router-dom';
-import * as common from './common';
+import { getUrl, isAdmin } from './common';
   
 const Navigation = () => {
 
-	const [logClass, setLogClass] = useState("li li--nav-active");
-	const [fileClass, setFileClass] = useState("li li--nav-inactive");
-	const [monitorClass, setMonitorClass] = useState("li li--nav-inactive");
-
 	const location = useLocation();
+	
+	if(!isAdmin()) {
 
-	useEffect(() => {
-
-		switch(location.pathname) {
-
-			case "/log":
-				setLogClass("li li--nav-active");
-				setFileClass("li li--nav-inactive");
-				setMonitorClass("li li--nav-inactive");
-				break;
-
-			case "/file":
-				setLogClass("li li--nav-inactive");
-				setFileClass("li li--nav-active");
-				setMonitorClass("li li--nav-inactive");
-				break;
-
-			case "/monitor":
-				setLogClass("li li--nav-inactive");
-				setFileClass("li li--nav-inactive");
-				setMonitorClass("li li--nav-active");
-				break;
-
-			default:
-				setLogClass("li li--nav-active");
-				setFileClass("li li--nav-inactive");
-				setMonitorClass("li li--nav-inactive");
-				break;
-		}
-
-	}, [location.pathname]);
-
-	const title = <li className="li li--nav-title"><a href={common.getUrl()} >park108.net</a></li>;
-	const log = common.isAdmin() ? <li className={logClass}><Link to="/log" >log</Link></li> : "";
-	const file = common.isAdmin() ? <li className={fileClass}><Link to="/file" >file</Link></li> : "";
-	const monitor = common.isAdmin() ? <li className={monitorClass}><Link to="/monitor" >mon</Link></li> : "";
+		return (
+			<nav className="nav nav--nav-bar">
+				<ul className="ul ul--nav-tabs">
+					<li className="li li--nav-title">
+						<a href={getUrl()}>park108.net</a>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 
 	return (
 		<nav className="nav nav--nav-bar">
 			<ul className="ul ul--nav-tabs">
-				{title}
-				{log}
-				{file}
-				{monitor}
+				<li className="li li--nav-title">
+					<a href={getUrl()}>park108.net</a>
+				</li>
+				<li className={"/log" === location.pathname ? "li li--nav-active" : "li li--nav-inactive"}>
+					<Link to="/log">log</Link>
+				</li>
+				<li className={"/file" === location.pathname ? "li li--nav-active" : "li li--nav-inactive"}>
+					<Link to="/file">file</Link>
+				</li>
+				<li className={"/monitor" === location.pathname ? "li li--nav-active" : "li li--nav-inactive"}>
+					<Link to="/monitor">mon</Link>
+				</li>
 			</ul>
 		</nav>
 	);
