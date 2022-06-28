d770248098b756f7b8fe524105b83560324e3595
Jongkil Park <park108@gmail.com>
2022-06-28 20:33:46 +0900

    Add test case for ImageSelector

--- src/Image/ImageSelector.js
+++ src/Image/ImageSelector.js
@@ -25,6 +25,7 @@ const ImageSelector = (props) => {
 		try {
 			const res = await getImages();
 			const retrieved = await res.json();
+			console.log(JSON.stringify(retrieved))
 
 			if(undefined !== retrieved.errorType) {
 				console.error(retrieved);
