a48a334d003953bda34bec89210313098992361a
Jongkil Park <park108@gmail.com>
2022-08-14 21:57:42 +0900

    Add test cases for 100% coverage of Navigation

--- src/__test__/Navigation.test.js
+++ src/__test__/Navigation.test.js
@@ -7,17 +7,28 @@ import * as common from '../common/common';
 describe('render navigation menu correctly', () => {
 
 	const history = createMemoryHistory();
+	const prevLocation = window.location;
+	const activatedListItemClass = "li li--nav-active";
 
 	it('render title menu correctly', () => {
 
-		history.push({location: {pathname: "/log"}});
+		// Mocking location
+		const location = new URL('https://www.park108.net/log')
+		location.assign = jest.fn()
+		location.replace = jest.fn()
+		location.reload = jest.fn()
+
+		delete window.location;
+		window.location = location;
+
+		history.push(location);
 
 		// Mocking login and admin check
 		common.isLoggedIn = jest.fn().mockResolvedValue(false);
 		common.isAdmin = jest.fn().mockResolvedValue(false);
 
 		render(
-			<Router location={history.location} navigator={history}>
+			<Router location={location} navigator={history}>
 				<Navigation />
 			</Router>
 		);
@@ -28,51 +39,91 @@ describe('render navigation menu correctly', () => {
 		expected.innerHTML = "park108.net";
 
 		expect(expected).toStrictEqual(html);
+
+		window.location = prevLocation;
 	});
 
 	it('render file menu correctly', () => {
 
-		history.push({location: {pathname: "/file"}});
+		// Mocking location
+		const location = new URL('https://www.park108.net/file')
+		location.assign = jest.fn()
+		location.replace = jest.fn()
+		location.reload = jest.fn()
+
+		delete window.location;
+		window.location = location;
+
+		history.push(location);
 
 		// Mocking login and admin check
 		common.isLoggedIn = jest.fn().mockResolvedValue(true);
 		common.isAdmin = jest.fn().mockResolvedValue(true);
 
 		render(
-			<Router location={history.location} navigator={history}>
+			<Router location={location} navigator={history}>
 				<Navigation />
 			</Router>
 		);
-
-		const html = screen.getByText("file").closest('a');
+		
+		// Is a anchor tag exist?
+		const anchor = screen.getByText("file").closest('a');
 
 		const expected = document.createElement("a");
 		expected.setAttribute("href", "/file");
 		expected.innerHTML = "file";
 
-		expect(expected).toStrictEqual(html);
+		expect(expected).toStrictEqual(anchor);
+		
+		// Is this menu's class active now?
+		const li = anchor.parentNode;
+		const liClass = li.getAttribute("class");
+
+		expect(liClass).toStrictEqual(activatedListItemClass);
+		
+		// Revert location
+		window.location = prevLocation;
 	});
 
-	it('render monitor menu correctly', () => {
+	it('render monitor menu correctly', async () => {
 
-		history.push({location: {pathname: "/monitor"}});
+		// Mocking location
+		const location = new URL('https://www.park108.net/monitor')
+		location.assign = jest.fn()
+		location.replace = jest.fn()
+		location.reload = jest.fn()
+
+		delete window.location;
+		window.location = location;
+
+		history.push(location);
 
 		// Mocking login and admin check
 		common.isLoggedIn = jest.fn().mockResolvedValue(true);
 		common.isAdmin = jest.fn().mockResolvedValue(true);
 
 		render(
-			<Router location={history.location} navigator={history}>
+			<Router location={location} navigator={history}>
 				<Navigation />
 			</Router>
 		);
-
-		const html = screen.getByText("mon").closest('a');
+		
+		// Is a anchor tag exist?
+		const anchor = screen.getByText("mon").closest('a');
 
 		const expected = document.createElement("a");
 		expected.setAttribute("href", "/monitor");
 		expected.innerHTML = "mon";
 
-		expect(expected).toStrictEqual(html);
+		expect(expected).toStrictEqual(anchor);
+		
+		// Is this menu's class active now?
+		const li = anchor.parentNode;
+		const liClass = li.getAttribute("class");
+
+		expect(liClass).toStrictEqual(activatedListItemClass);
+		
+		// Revert location
+		window.location = prevLocation;
 	});
 });
