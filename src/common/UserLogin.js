4dca0c21ca444d1cc6cb26dc996baa82e954a321
Jongkil Park <park108@gmail.com>
2022-12-06 21:47:08 +0900

    Start enhance UserLogin

--- src/common/UserLogin.js
+++ src/common/UserLogin.js
@@ -1,30 +1,26 @@
 import React from "react";
-// import { useLocation } from 'react-router-dom';
 import * as common from '../common/common';
 
-export const getLoginUrl = (redirectUri) => {
+export const getLoginUrl = () => {
 	if ('production' === process.env.NODE_ENV) {
-		return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https://park108.net" + redirectUri;
+		return "https://log.auth.ap-northeast-2.amazoncognito.com/login?client_id=5obtheulb7olv5uhnkubuldgqj&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https://park108.net";
 	}
 	else if ('development' === process.env.NODE_ENV) {
-		return "https://log-dev.auth.ap-northeast-2.amazoncognito.com/login?client_id=h3m92a27t39sfcat302tiqtko&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000" + redirectUri;
+		return "https://log-dev.auth.ap-northeast-2.amazoncognito.com/login?client_id=h3m92a27t39sfcat302tiqtko&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000";
 	}
 }
 
-export const getLogoutUrl = (logoutUri) => {
+export const getLogoutUrl = () => {
 
 	if ('production' === process.env.NODE_ENV) {
-		return "https://log.auth.ap-northeast-2.amazoncognito.com/logout?client_id=5obtheulb7olv5uhnkubuldgqj&logout_uri=https://park108.net" + logoutUri;
+		return "https://log.auth.ap-northeast-2.amazoncognito.com/logout?client_id=5obtheulb7olv5uhnkubuldgqj&logout_uri=https://park108.net";
 	}
 	else if ('development' === process.env.NODE_ENV) {
-		return "https://log-dev.auth.ap-northeast-2.amazoncognito.com/logout?client_id=h3m92a27t39sfcat302tiqtko&logout_uri=http://localhost:3000" + logoutUri;
+		return "https://log-dev.auth.ap-northeast-2.amazoncognito.com/logout?client_id=h3m92a27t39sfcat302tiqtko&logout_uri=http://localhost:3000";
 	}
 }
 
 const UserLogin = () => {
-
-	// const location = useLocation();
-	// console.log(location);
 
 	return (
 		<span
@@ -38,10 +34,10 @@ const UserLogin = () => {
 				// TOOD: 현재 페이지로 리다이렉트
 				if(common.isLoggedIn()) {
 					common.deleteCookie("access_token");
-					window.location.href = getLogoutUrl("");
+					window.location.href = getLogoutUrl();
 				}
 				else {
-					window.location.href = getLoginUrl("");
+					window.location.href = getLoginUrl();
 				}
 			}}
 		>
