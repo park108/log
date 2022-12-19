264d735c89ab05f0d91e2d9f28814700231c5cd5
Jongkil Park <park108@gmail.com>
2022-12-19 21:49:32 +0900

    Refactor ImageItem

--- src/Image/ImageItem.js
+++ src/Image/ImageItem.js
@@ -2,35 +2,10 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './ImageSelector.css';
 
-const enlargeImage = (fullsizeImageUrl, e) => {
-	e.target.setAttribute("enlarged", "Y");
-	e.target.setAttribute("src", fullsizeImageUrl);
-	e.target.setAttribute("class", "img img--image-imageitem img--image-selected");
-}
-
-const shrinkImage = (thumbnailImageUrl, e) => {
-	e.target.setAttribute("enlarged", "N");
-	e.target.setAttribute("src", thumbnailImageUrl);
-	e.target.setAttribute("class", "img img--image-imageitem");
-}
-
 const ImageItem = (props) => {
 
 	const thumbnailImageUrl = props.url;
 	const fullsizeImageUrl = thumbnailImageUrl.replace("thumbnail/", "");
-
-	const clickImage = (e) => {
-	
-		const isEnlarged = ("Y" === e.target.getAttribute("enlarged"));
-	
-		if(isEnlarged) {
-			props.copyMarkdownString(e);
-			shrinkImage(thumbnailImageUrl, e);
-		}
-		else {
-			enlargeImage(fullsizeImageUrl, e);
-		}
-	}
 
 	return (
 		<img className="img img--image-imageitem"
@@ -42,7 +17,22 @@ const ImageItem = (props) => {
 			imageurl={fullsizeImageUrl}
 			thumbnailurl={thumbnailImageUrl}
 			enlarged={"N"}
-			onClick={clickImage}
+			onClick={(e) => {
+	
+				const isEnlarged = ("Y" === e.target.getAttribute("enlarged"));
+			
+				if(isEnlarged) {
+					props.copyMarkdownString(e);
+					e.target.setAttribute("enlarged", "N");
+					e.target.setAttribute("src", thumbnailImageUrl);
+					e.target.setAttribute("class", "img img--image-imageitem");
+				}
+				else {
+					e.target.setAttribute("enlarged", "Y");
+					e.target.setAttribute("src", fullsizeImageUrl);
+					e.target.setAttribute("class", "img img--image-imageitem img--image-selected");
+				}
+			}}
 		/>
 	);
 }
