38e6446cad332407c2829f6faaeac16c6cd47adc
Jongkil Park <park108@gmail.com>
2022-12-29 21:54:18 +0900

    Refactor navigation

--- src/common/Navigation.js
+++ src/common/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from 'react-router-dom';
 import { getUrl, isAdmin } from './common';
 import SearchInput from '../Search/SearchInput';
@@ -12,17 +12,25 @@ const ADMIN_MENU = [
 const Navigation = () => {
 
 	const location = useLocation();
+	const [adminMenu, setAdminMenu] = useState();
 
-	const adminMenu = isAdmin() ? (
-		ADMIN_MENU.map((item) => (
-			<li
-				key={ item.name }
-				className={ location.pathname.startsWith(item.path) ? "li li--nav-active" : "li li--nav-inactive" }
-			>
-				<Link to={ item.path }>{ item.name }</Link>
-			</li>
-		))
-	) : "";
+	useEffect(() => {
+		if(isAdmin()) {
+			
+			const path = location.pathname;
+
+			setAdminMenu(
+				ADMIN_MENU.map((item) => (
+					<li
+						key={ item.name }
+						className={ path.startsWith(item.path) ? "li li--nav-active" : "li li--nav-inactive" }
+					>
+						<Link to={ item.path }>{ item.name }</Link>
+					</li>
+				))
+			);
+		}
+	}, [location.pathname]);
 
 	return (
 		<nav className="nav nav--nav-bar">
